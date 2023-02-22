266c66f1aeecfb593c3502de450292d58a8e8cd8
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-02-22 17:09:25 +0100

    Simplify test case

--- server/src/tests/simulations.test.ts
+++ server/src/tests/simulations.test.ts
@@ -1,17 +1,14 @@
 import App from '@/app';
-import { CreateSimulationDto } from '@/dtos/simulations.dto';
-import userModel from '@/models/users.model';
+import {CreateSimulationDto} from '@/dtos/simulations.dto';
 import SimulationRoute from '@/routes/simulations.route';
-import UsersRoute from '@/routes/users.route';
 import request from 'supertest';
 
 afterAll(async () => {
   await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
 });
 
-let SIMULATION_ID: number;
 describe('Simulation is', () => {
-  it('created', () => {
+  it('created, started and running', () => {
     const simulationRoute = new SimulationRoute();
     const app = new App([simulationRoute]);
 
@@ -27,39 +24,18 @@ describe('Simulation is', () => {
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json');
 
-    return newSimulationResponse
-      .expect(201, {
-        data: {
-          id: 4,
-          teamResults: [
-            { team: 'Poland', score: 0 },
-            { team: 'Germany', score: 0 },
-          ],
-          inProgress: false,
-          ticksLeft: 2,
-          ticksEverySeconds: 1,
-        },
-        message: 'created',
-      })
-      .then(res => {
-        SIMULATION_ID = res.body.id;
-      });
-    //
-    // const getSimulationStatusResponse = request(app.getServer())
-    //   .get(`${simulationRoute.path}/4`)
-    // return getSimulationStatusResponse.expect(201, {
-    //   data: {
-    //     id: 4,
-    //     teamResults: [
-    //       { team: 'Poland', score: 0 },
-    //       { team: 'Germany', score: 0 },
-    //     ],
-    //   },
-    //   message: 'created',
-    // });
-
-    // const startSimulationResponse =
-
-    // return request(app.getServer()).get(`${simulationRoute.path}`).expect(200, { data: findUser, message: 'findAll' });
+    return newSimulationResponse.expect(201, {
+      data: {
+        id: 4, // this test assumes there are already 3 simulations in the database
+        teamResults: [
+          { team: 'Poland', score: 0 },
+          { team: 'Germany', score: 0 },
+        ],
+        inProgress: false,
+        ticksLeft: 2,
+        ticksEverySeconds: 1,
+      },
+      message: 'created',
+    });
   });
 });
